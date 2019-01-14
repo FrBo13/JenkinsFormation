abff0c7f3f599fd31a3aa031914c1f616e3ddcff
Francky <Francky@mail>
2019-01-14 10:28:36 +0100

    Initialisation

---
+++ src/main/java/Formation_DMR/fbo/App.java
@@ -0,0 +1,16 @@
+package Formation_DMR.fbo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i=1;i<5;i++)
+    	{
+    		System.out.println( "Hello World! "+i );
+    	}
+    }
+}
